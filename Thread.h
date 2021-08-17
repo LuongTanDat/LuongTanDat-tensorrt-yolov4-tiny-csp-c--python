9d7939ce58f02cb3384a902a4f4de2ed392cb720
longvv <longvv@kikaitech.io>
2021-08-18 03:31:03 +0700

    push create tabular learner dataset

--- Thread.h
+++ Thread.h
@@ -40,58 +40,29 @@
 
 namespace M
 {
-#ifdef JSON
-#ifdef INFERENCE_DARKNET
-    nlohmann::json bbox_to_json(bbox_t &res)
-    {
-        nlohmann::json j;
-        j["bbox"] = nlohmann::json::array({res.x, res.y, res.w, res.h});
-        j["cls"] = res.obj_id;
-        j["prob"] = res.prob;
-#ifdef DEBUG
-        std::cout << "[DEBUG][JSON] " << j << std::endl;
-#endif // DEBUG
-        return j;
-    }
-#else
     nlohmann::json bbox_to_json(YOLOv4::DetectRes &res)
     {
         nlohmann::json j;
         j["bbox"] = nlohmann::json::array({res.x - res.w / 2, res.y - res.h / 2, res.w, res.h});
         j["cls"] = res.classes;
         j["prob"] = res.prob;
-#ifdef DEBUG
-        std::cout << "[DEBUG][JSON] " << j << std::endl;
-#endif // DEBUG
+        j["feat"] = {};
+        for (int i = 0; i < 7; i++)
+        {
+            j["feat"].push_back(*(res.feature + i));
+        }
         return j;
     }
-#endif // INFERENCE_DARKNET
 
-#ifdef INFERENCE_DARKNET
-    nlohmann::json res_to_json(std::vector<bbox_t> &vecBBox
-#else
-    nlohmann::json res_to_json(std::vector<YOLOv4::DetectRes> &vecBBox
-#endif // INFERENCE_DARKNET
-#ifdef INFERENCE_ALPHAPOSE_TORCH
-                           ,
-                           std::vector<PoseKeypoints> &vecKp
-#endif // INFERENCE_ALPHAPOSE_TORCH
-    )
+    nlohmann::json res_to_json(std::vector<YOLOv4::DetectRes> &vecBBox, std::vector<PoseKeypoints> &vecKp)
     {
         nlohmann::json j = {};
-#ifdef INFERENCE_ALPHAPOSE_TORCH
         int countKp = 0;
-#endif // INFERENCE_ALPHAPOSE_TORCH
         for (int i = 0; i < vecBBox.size(); i++)
         {
             nlohmann::json jx;
             jx["det"] = bbox_to_json(vecBBox[i]);
-#ifdef INFERENCE_ALPHAPOSE_TORCH
-#ifdef INFERENCE_DARKNET
-            if (vecBBox[i].obj_id > 4)
-#else
             if (vecBBox[i].classes > 4)
-#endif // INFERENCE_DARKNET
             {
                 jx["pose"] = nlohmann::json::value_t::null;
             }
@@ -100,46 +71,20 @@ namespace M
                 jx["pose"] = vecKp[countKp].to_json();
                 countKp++;
             }
-#endif // INFERENCE_ALPHAPOSE_TORCH
             j.push_back(jx);
         }
         return j;
     }
-#endif // JSON
 
-#ifdef INFERENCE_ALPHAPOSE_TORCH
-#ifdef INFERENCE_DARKNET
-    void convert_DetectRes_bbox(const bbox_t &res, bbox &out)
-#else
     void convert_DetectRes_bbox(const YOLOv4::DetectRes &res, bbox &out)
-#endif // INFERENCE_DARKNET
     {
-#ifdef INFERENCE_DARKNET
-        out.rect.x = (float)res.x;
-        out.rect.y = (float)res.y;
-#else
         out.rect.x = (float)(res.x - res.w / 2);
         out.rect.y = (float)(res.y - res.h / 2);
-#endif // INFERENCE_DARKNET
         out.rect.width = (float)res.w;
         out.rect.height = (float)res.h;
         out.score = (float)res.prob;
     }
 
-#ifdef INFERENCE_DARKNET
-    void convert_vecDetectRes_vecbbox(const std::vector<bbox_t> &vecRes, std::vector<bbox> &conv)
-    {
-        for (bbox_t res : vecRes)
-        {
-            if (res.obj_id > 4)
-                continue;
-
-            bbox outres;
-            convert_DetectRes_bbox(res, outres);
-            conv.push_back(outres);
-        }
-    }
-#else
     void convert_vecDetectRes_vecbbox(const std::vector<YOLOv4::DetectRes> &vecRes, std::vector<bbox> &conv)
     {
         for (YOLOv4::DetectRes res : vecRes)
@@ -152,22 +97,14 @@ namespace M
             conv.push_back(outres);
         }
     }
-#endif // INFERENCE_DARKNET
-#endif // INFERENCE_ALPHAPOSE_TORCH
 
     // TODO: Minimize, optimize this
     struct pipeline_data
     {
         cv::Mat cap_frame;
         // cv::Mat frame_from_cam[4];
-#ifdef INFERENCE_DARKNET
-        std::map<int, std::vector<bbox_t>> result;
-#else
         std::map<int, std::vector<YOLOv4::DetectRes>> result;
-#endif // INFERENCE_DARKNET
-#ifdef INFERENCE_ALPHAPOSE_TORCH
         std::map<int, std::vector<PoseKeypoints>> poseKeypoints;
-#endif // INFERENCE_ALPHAPOSE_TORCH
         std::string uuid;
         pipeline_data(){};
         pipeline_data(cv::Mat frame) : cap_frame(frame){};
